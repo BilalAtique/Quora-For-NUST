b63b980d0ae6964daa42ab0f74a7b0ac922524eb
MuhammadBilalBinAtique <bilalbinatique@gmail.com>
2022-01-19 17:16:36 +0500

    Basic app setting

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Quora for Nust</h2>
+      <p>A place to ask questions</p>
+    </div>
+  );
+}
+
+export default App;
