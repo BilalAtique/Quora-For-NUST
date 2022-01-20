b0c62ebba07d69b0dee0b16d2b8d5ef44ce33622
MuhammadBilalBinAtique <bilalbinatique@gmail.com>
2022-01-20 22:04:58 +0500

    Setting up react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import Register from './components/registration/Registration';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/registration/Registration';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </Router>,
     </div>
   );
 }
