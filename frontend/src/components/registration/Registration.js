3e9540e0905f6612c106573b0d9c651b07c081fd
MuhammadBilalBinAtique <bilalbinatique@gmail.com>
2022-01-20 22:56:48 +0500

    Modifyng signup page to be able to send data to express server

--- frontend/src/components/registration/Registration.js
+++ frontend/src/components/registration/Registration.js
@@ -1,36 +1,57 @@
+import { useState } from "react";
+
 const Register = () => {
+    const [name, setName] = useState('');
+    const [batchInfo, setBatchInfo] = useState('');
+    const [password, setPassword] = useState('');
+    const [confPassword, setConfPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = { name, batchInfo, password, confPassword };
+    
+        fetch('http://localhost:8000/user/', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(user)
+        }).then(() => {
+          console.log('new User Registerd');
+          console.log(user);
+        })
+      }
+    
+
     return (
         <>
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
                         <div className="signup-form">
-                            <form action="" className="mt-5 border p-4 bg-light shadow">
+                            <form onSubmit={handleSubmit} className="mt-5 border p-4 bg-light shadow">
                                 <h4 className="mb-5 text-secondary">Create Your Account</h4>
                                 <div className="row">
                                     <div className="mb-3 col-md-12">
                                         <label>Name<span className="text-danger">*</span></label>
-                                        <input type="text" name="name" className="form-control" placeholder="Enter your Name" />
-                                    </div>
-
-                                    {/* <div className="mb-3 col-md-6">
-                                        <label>Last Name<span className="text-danger">*</span></label>
-                                        <input type="text" name="lname" className="form-control" placeholder="Enter Last Name" />
-                                    </div> */}
-
-                                    <div className="mb-3 col-md-12">
-                                        <label>Password<span className="text-danger">*</span></label>
-                                        <input type="password" name="password" className="form-control" placeholder="Enter Password" />
-                                    </div>
-
-                                    <div className="mb-3 col-md-12">
-                                        <label>Confirm Password<span className="text-danger">*</span></label>
-                                        <input type="password" name="confirm-password" className="form-control" placeholder="Confirm Password" />
+                                        <input required type="text" name="name" onChange={(e) => setName(e.target.value)}
+                                            value={name} className="form-control" placeholder="Enter your Name" />
                                     </div>
 
                                     <div className="mb-3 col-md-12">
                                         <label>Batch info<span className="text-danger">*</span></label>
-                                        <input type="text" name="batch-info" className="form-control" placeholder="Sample: BSE-27B" />
+                                        <input required type="text" name="batch-info" onChange={(e) => setBatchInfo(e.target.value)}
+                                            value={batchInfo} className="form-control" placeholder="Sample: BSE-27B" />
+                                    </div>
+
+                                    <div className="mb-3 col-md-12">
+                                        <label>Password<span className="text-danger">*</span></label>
+                                        <input required type="password" name="password" onChange={(e) => setPassword(e.target.value)}
+                                            value={password} className="form-control" placeholder="Enter Password" />
+                                    </div>
+
+                                    <div className="mb-3 col-md-12">
+                                        <label>Confirm Password<span className="text-danger">*</span></label>
+                                        <input required type="password" name="confirm-password" onChange={(e) => setConfPassword(e.target.value)}
+                                            value={confPassword} className="form-control" placeholder="Confirm Password" />
                                     </div>
 
                                     <div className="col-md-12">
@@ -43,7 +64,6 @@ const Register = () => {
                     </div>
                 </div>
             </div>
-
         </>
     );
 }
